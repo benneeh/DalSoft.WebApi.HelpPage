b4da8534e5d86ae9a49faffc7a4ce47de38be4c5
Benjamin Bleher <b.bleher@gmx.net>
2016-05-22 01:31:28 +0200

    Revert "- Added viewsRelativePath param to "UseWebApiHelpPage" - changed "UseWebApiHelpPage" to use Path.Combine instead of +"
    This reverts commit ed1d78bb74a486c80b178d3b79aa987763e13e68.

--- DalSoft.WebApi.HelpPage/HelpPageMiddlewareExtensions.cs
+++ DalSoft.WebApi.HelpPage/HelpPageMiddlewareExtensions.cs
@@ -10,20 +10,19 @@ namespace DalSoft.WebApi.HelpPage
 {
     public static class HelpPageMiddlewareExtensions
     {
-        public static void UseWebApiHelpPage(this IAppBuilder app, HttpConfiguration httpConfiguration, string helpRoute = null,
-            string viewsPhysicalPath = null, string viewsRelativePath = null, string xmlDocumentationPhysicalPath = null, dynamic viewBag = null)
+        public static void UseWebApiHelpPage(this IAppBuilder app, HttpConfiguration httpConfiguration, string helpRoute=null, string viewsPhysicalPath=null, string xmlDocumentationPhysicalPath=null, dynamic viewBag=null)
         {
             const string defaultRoute = "help";
             var executingAssemblyPath = GetBinPath();
             var executingAssemblyName = Assembly.GetCallingAssembly().GetName().Name;
-
+            
             helpRoute = helpRoute ?? defaultRoute;
 
-            viewsPhysicalPath = viewsPhysicalPath ?? Path.Combine(executingAssemblyPath.Replace("file:\\", string.Empty), (viewsRelativePath ?? "Help\\DalSoft.WebApi.HelpPage.Views"));
-            xmlDocumentationPhysicalPath = xmlDocumentationPhysicalPath ?? Path.Combine(executingAssemblyPath, executingAssemblyName + ".xml");
+            viewsPhysicalPath = viewsPhysicalPath ?? executingAssemblyPath.Replace("file:\\", string.Empty) + "\\Help\\DalSoft.WebApi.HelpPage.Views";
+            xmlDocumentationPhysicalPath = xmlDocumentationPhysicalPath ?? executingAssemblyPath + "//" + executingAssemblyName + ".xml";
             IDictionary<string, object> viewBagDictionary = DynamicToDictionary(viewBag);
-
-            if (helpRoute.StartsWith("/"))
+            
+            if (helpRoute.StartsWith("/"))  
                 helpRoute = helpRoute.Substring(1, helpRoute.Length - 1);
 
             viewBagDictionary.Add("HelpRoute", helpRoute);
@@ -45,8 +44,8 @@ namespace DalSoft.WebApi.HelpPage
             IDictionary<string, object> dictionary = new Dictionary<string, object>();
 
             if (o == null) return dictionary;
-
-            foreach (PropertyDescriptor property in TypeDescriptor.GetProperties(o.GetType())) dictionary.Add(property.Name, property.GetValue(o));
+            
+            foreach (PropertyDescriptor property in TypeDescriptor.GetProperties(o.GetType()))  dictionary.Add(property.Name, property.GetValue(o));
             return dictionary;
         }
     }
